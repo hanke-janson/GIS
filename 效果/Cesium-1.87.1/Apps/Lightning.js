dd0629c57a7c9b0c2850c6de721d94bec3c28cb2
xi yuan <xiyuan@gk-estor.com>
2023-01-11 10:20:20 +0800

    局部闪电

--- 效果/Cesium-1.87.1/Apps/Lightning.js
+++ 效果/Cesium-1.87.1/Apps/Lightning.js
@@ -1,79 +1,149 @@
-// 闪电
-`float hash(float x)
-{
-  return fract(21654.6512 * sin(385.51 * x));
+//闪电特效
+export class lightningEffect {
+  /**
+   * @param {viewer对象} viewer
+   * @param {粒子位置} position
+   * @param {粒子大小} particleSize
+   * @param {粒子系统的存活时间} lifetime
+   * @param {发射器半径} radius
+   * @param {效果是否循环} loop
+   */
+  constructor(viewer, position, particleSize, lifetime, radius, loop) {
+    this.viewer = viewer;
+    this.viewModel = {
+      emissionRate: 0.5,
+      // 每个粒子显示时间
+      minimumParticleLife: 0.1,
+      maximumParticleLife: 0.5,
+      minimumSpeed: 1.0, //粒子发射的最小速度
+      maximumSpeed: 3.0, //粒子发射的最大速度
+      startScale: 5.0,
+      endScale: 5.0,
+      particleSize: particleSize,
+      lifetime: lifetime,
+      loop: loop,
+    };
+    // 角度
+    this.radius = radius;
+    this.emitterModelMatrix = new Cesium.Matrix4();
+    this.translation = new Cesium.Cartesian3();
+    this.rotation = new Cesium.Quaternion();
+    this.hpr = new Cesium.HeadingPitchRoll();
+    this.trs = new Cesium.TranslationRotationScale();
+    this.scene = this.viewer.scene;
+    this.particleSystem = "";
+    this.entity = this.viewer.entities.add({
+      //选择粒子放置的坐标
+      position: position,
+    });
+    this.init();
+  }
+  init() {
+    const _this = this;
+    this.viewer.clock.shouldAnimate = true;
+    this.viewer.scene.globe.depthTestAgainstTerrain = false;
+    this.viewer.trackedEntity = this.entity;
+    var particleSystem = this.scene.primitives.add(
+      new Cesium.ParticleSystem({
+        image: "./蓝色发光闪电特效.png", //生成所需粒子的图片路径
+        // image: require("./img/explot.png"), //生成所需粒子的图片路径
+        //粒子在生命周期开始时的颜色
+        // startColor: new Cesium.Color(1, 1, 1, 1),
+        startColor: Cesium.Color.BLUE.withAlpha(0.7),
+        //粒子在生命周期结束时的颜色
+        endColor: Cesium.Color.BLUE.withAlpha(0.2),
+        // endColor: Cesium.Color.YELLOW.withAlpha(0.3),
+        //粒子在生命周期开始时初始比例
+        startScale: _this.viewModel.startScale,
+        //粒子在生命周期结束时比例
+        endScale: _this.viewModel.endScale,
+        //粒子发射的最小速度
+        minimumParticleLife: _this.viewModel.minimumParticleLife,
+        //粒子发射的最大速度
+        maximumParticleLife: _this.viewModel.maximumParticleLife,
+        //粒子质量的最小界限
+        minimumSpeed: _this.viewModel.minimumSpeed,
+        //粒子质量的最大界限
+        maximumSpeed: _this.viewModel.maximumSpeed,
+        //以像素为单位缩放粒子图像尺寸
+        imageSize: new Cesium.Cartesian2(
+          _this.viewModel.particleSize,
+          _this.viewModel.particleSize
+        ),
+        //每秒发射的粒子数
+        emissionRate: _this.viewModel.emissionRate,
+        // 默认爆炸不循环
+        loop: _this.viewModel.loop ? _this.viewModel.loop : false,
+        //粒子系统发射粒子的时间（秒）
+        lifetime: _this.viewModel.lifetime,
+        //设置粒子的大小是否以米或像素为单位
+        sizeInMeters: true,
+        //系统的粒子发射器
+        emitter: new Cesium.CircleEmitter(Cesium.Math.toRadians(_this.radius)), //BoxEmitter 盒形发射器，ConeEmitter 锥形发射器，SphereEmitter 球形发射器，CircleEmitter圆形发射器
+      })
+    );
+    this.particleSystem = particleSystem;
+    this.preUpdateEvent();
+  }
+
+  //场景渲染事件
+  preUpdateEvent() {
+    let _this = this;
+    this.viewer.scene.preUpdate.addEventListener(function (scene, time) {
+      //发射器地理位置
+      _this.particleSystem.modelMatrix = _this.computeModelMatrix(
+        _this.entity,
+        time
+      );
+      //发射器局部位置
+      _this.particleSystem.emitterModelMatrix = _this.computeEmitterModelMatrix();
+      // 将发射器旋转
+      if (_this.viewModel.spin) {
+        _this.viewModel.heading += 1.0;
+        _this.viewModel.pitch += 1.0;
+        _this.viewModel.roll += 1.0;
+      }
+    });
+  }
+
+  computeModelMatrix(entity, time) {
+    return entity.computeModelMatrix(time, new Cesium.Matrix4());
+  }
+
+  computeEmitterModelMatrix() {
+    this.hpr = Cesium.HeadingPitchRoll.fromDegrees(0.0, 0.0, 0.0, this.hpr);
+    this.trs.translation = Cesium.Cartesian3.fromElements(
+      -4.0,
+      0.0,
+      1.4,
+      this.translation
+    );
+    this.trs.rotation = Cesium.Quaternion.fromHeadingPitchRoll(
+      this.hpr,
+      this.rotation
+    );
+
+    return Cesium.Matrix4.fromTranslationRotationScale(
+      this.trs,
+      this.emitterModelMatrix
+    );
+  }
+
+  removeEvent() {
+    this.viewer.scene.preUpdate.removeEventListener(this.preUpdateEvent, this);
+    this.emitterModelMatrix = undefined;
+    this.translation = undefined;
+    this.rotation = undefined;
+    this.hpr = undefined;
+    this.trs = undefined;
+  }
+
+  //移除粒子特效
+  remove() {
+    () => {
+      return this.removeEvent();
+    }; //清除事件
+    this.viewer.scene.primitives.remove(this.particleSystem); //删除粒子对象
+    this.viewer.entities.remove(this.entity); //删除entity
+  }
 }
-float hash(vec2 p)
-{
-  return fract(1654.65157 * sin(15.5134763 * p.x + 45.5173247 * p.y + 5.21789));
-}
-vec2 hash2(vec2 p)
-{
-  return vec2(hash(p * .754), hash(1.5743 * p + 4.5476351));
-}
-vec2 add = vec2(1.0, 0.0);
-vec2 noise2(vec2 x)
-{
-vec2 p = floor(x);
-vec2 f = fract(x);
-  f = f * f * (3.0 - 2.0 * f);
-vec2 res = mix(mix(hash2(p),
-hash2(p + add.xy), f.x),
-mix(hash2(p + add.yx), hash2(p + add.xx), f.x), f.y);
-  return res;
-}
-vec2 fbm2(vec2 x)
-{
-vec2 r = vec2(0.0);
-float a = 1.0;
-  for (int i = 0; i < 8; i++)
-{
-r += noise2(x) * a;
-x *= 2.;
-a *= .5;
-}
-  return r;
-}
-float dseg(vec2 ba, vec2 pa)
-{
-float h = clamp(dot(pa, ba) / dot(ba, ba), -0.2, 1.);
-  return length(pa - ba * h);
-}
-uniform sampler2D colorTexture; 
-uniform float fall_interval; 
-uniform float mix_factor; 
-varying vec2 v_textureCoordinates; 
-void main(void){
-vec2 uv = gl_FragCoord.xy; 
-float iTime = czm_frameNumber * fall_interval * clamp(fall_interval * 0.1, 0.01, 0.1); 
-vec2 p = uv / czm_viewport.zw; 
-vec2 d; 
-vec2 tgt = vec2(1., -1.); 
-float c = 0.; 
-  if (p.y >= 0.) 
-  c = (1. - (fbm2((p + .2) * p.y + .1 * iTime)).x) * p.y; 
-else 
-  c = (1. - (fbm2(p + .2 + .1 * iTime)).x) * p.y * p.y; 
-vec3 col = vec3(0.); 
-//\u7528\u5206\u5E03\u6717\u8FD0\u52A8\u6A21\u62DF\u4E4C\u4E91
-vec3 col1 = c * vec3(.3, .5, 1.); 
-float mdist = 100000.; 
-float t = hash(floor(5. * iTime)); 
-  tgt += 4. * hash2(tgt + t) - 1.5; 
-  if (hash(t + 2.3) > .6) 
-  for (int i = 0; i < 100; i++) {
-vec2 dtgt = tgt - p; 
-d = .05 * (vec2(-.5, -1.) + hash2(vec2(float(i), t))); 
-//\u70B9d\u5230\u9009\u6BB5dtgt\u7684\u8DDD\u79BB
-float dist = dseg(d, dtgt); 
-//\u6C42\u6700\u5C0F\u8DDD\u79BB(https://blog.csdn.net/weixin_43751272/article/details/114017953
-mdist = min(mdist, dist); 
-tgt -= d; 
-c = exp(-1.2 * dist) + exp(-55. * mdist); 
-col = c * vec3(.7, .8, 1.); 
-  } 
-  //\u52A0\u4E0A\u4E4C\u4E91
-  col += col1; 
-  gl_FragColor = mix(texture2D(colorTexture, v_textureCoordinates), vec4(col, 0.0), mix_factor); 
-} 
-`;
