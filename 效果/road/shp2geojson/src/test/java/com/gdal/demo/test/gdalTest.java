15134a6f8d3f46b210bb28ca4022b88fe9be91df
xiyuan <xiyuan@gk-estor.com>
2023-03-03 11:26:37 +0800

    gdaladdo

--- 效果/road/shp2geojson/src/test/java/com/gdal/demo/test/gdalTest.java
+++ 效果/road/shp2geojson/src/test/java/com/gdal/demo/test/gdalTest.java
@@ -5,9 +5,13 @@ import org.junit.Test;
 
 public class gdalTest {
     @Test
-    public void TestDemo() {
+    public void TestShp2GeoJson() {
         String shpPath = "C:\\Users\\admin\\Desktop\\drone\\anQuanQu.shp";
         String geojsonPath = "C:\\Users\\admin\\Desktop\\drone\\anQuanQu.geojson";
         GdalTools.shp2GeoJson(shpPath, geojsonPath);
+    }
+    @Test
+    public void TestGdaladdo(){
+
     }
 }
